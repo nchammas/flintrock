b8855dfe2a15396a6bdfe406333443cc8845dbe9
Nicholas Chammas <nicholas.chammas@gmail.com>
2015-04-10 23:26:17 -0400

    moar better copy

--- spark-health.sh
+++ spark-health.sh
@@ -3,7 +3,7 @@ set -e
 cat << EOM > test.py
 from pyspark import SparkContext
 sc = SparkContext()
-print "Cores: {c}".format(c=sc.defaultParallelism)
+print "Slave Cores: {c}".format(c=sc.defaultParallelism)
 EOM
 
-./spark/bin/spark-submit test.py 2>&1 | grep "^Cores"
+./spark/bin/spark-submit test.py 2>&1 | grep "^Slave Cores"
