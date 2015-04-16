80cd800e979d063913e8571534222445c8e8b6a6
Nicholas Chammas <nicholas.chammas@gmail.com>
2015-04-15 22:02:21 -0400

    better spark health check

--- spark-health.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-cat << EOM > test.py
-from pyspark import SparkContext
-sc = SparkContext()
-print "Slave Cores: {c}".format(c=sc.defaultParallelism)
-EOM
-
-./spark/bin/spark-submit test.py 2>&1 | grep "^Slave Cores"
