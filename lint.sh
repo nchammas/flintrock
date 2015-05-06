c9d528788aff3ecb0c3b61653cf6316fa4abf841
Nicholas Chammas <nicholas.chammas@gmail.com>
2015-05-06 00:02:47 -0400

    linter tweaks

--- lint.sh
+++ lint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-echo "Errors"
+# echo "Errors"
 python3 -m compileall ./flintrock
 python3 -m pep8 ./flintrock
 
 # Warnings. Don't fail the linter.
-echo "Warnings"
-python3 -m pep8 --select "E501" ./flintrock || true
+# echo "Warnings"
+# python3 -m pep8 --select "E501" ./flintrock || true
