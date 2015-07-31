529861f56df23636ee42fe06a22a8ba6df58377d
Nicholas Chammas <nicholas.chammas@gmail.com>
2015-07-31 00:21:11 -0400

    add stop/start to tests

--- test-integration.sh
+++ test-integration.sh
@@ -7,5 +7,7 @@ CLUSTER_NAME="integration-test"
 
 ./flintrock launch "$CLUSTER_NAME" --num-slaves 1
 ./flintrock describe "$CLUSTER_NAME"
-# ./flintrock login "$CLUSTER_NAME"  # How do you test this automatically? 
+./flintrock stop "$CLUSTER_NAME" --assume-yes
+./flintrock start "$CLUSTER_NAME"
+# ./flintrock login "$CLUSTER_NAME"  # How do you test this automatically?
 ./flintrock destroy "$CLUSTER_NAME" --assume-yes
