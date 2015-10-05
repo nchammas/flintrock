9adc5669a88ffa4e96cad049399ae0aca441bc44
Nicholas Chammas <nicholas.chammas@gmail.com>
2015-10-04 21:28:20 -0400

    clean up unused code

--- install-spark.sh
+++ install-spark.sh
@@ -9,7 +9,9 @@ echo "  distribution: ${distribution}"
 
 file="spark-${spark_version}-bin-${distribution}.tgz"
 
-curl --silent --remote-name "http://s3.amazonaws.com/spark-related-packages/${file}"
+curl --silent --remote-name "https://s3.amazonaws.com/spark-related-packages/${file}"
+gzip -t "$file"
+
 mkdir "spark"
 # strip-components puts the files in the root of spark/
 tar xzf "$file" -C "spark" --strip-components=1
@@ -17,31 +19,3 @@ rm "$file"
 
 sudo mkdir "{spark_scratch_dir}"
 sudo chown ec2-user:ec2-user "{spark_scratch_dir}"
-
-# if [[ "$SPARK_VERSION" == *\|* ]]
-# then
-#   mkdir spark
-#   pushd spark > /dev/null
-#   git init
-#   repo=`python -c "print '$SPARK_VERSION'.split('|')[0]"` 
-#   git_hash=`python -c "print '$SPARK_VERSION'.split('|')[1]"`
-#   git remote add origin $repo
-#   git fetch origin
-#   git checkout $git_hash
-#   sbt/sbt clean assembly
-#   sbt/sbt publish-local
-#   popd > /dev/null
-# else 
-#   if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-#     wget http://s3.amazonaws.com/spark-related-packages/spark-1.3.0-bin-hadoop1.tgz
-#   else
-#     wget http://s3.amazonaws.com/spark-related-packages/spark-1.3.0-bin-cdh4.tgz
-#   fi
-
-#   echo "Unpacking Spark"
-#   tar xvzf spark-*.tgz > /tmp/spark-ec2_spark.log
-#   rm spark-*.tgz
-#   mv `ls -d spark-* | grep -v ec2` spark
-# fi
-
-# exit 0
