11f1245bece87cd3aff429ed7f4a106ad429ad2f
Nicholas Chammas <nicholas.chammas@gmail.com>
2015-10-27 16:33:17 -0400

    fail on first problem

--- lint.sh
+++ lint.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-# echo "Errors"
+set -e
+
 python3 -m compileall ./flintrock.py
 python3 -m pep8 ./flintrock.py
-
-# Warnings. Don't fail the linter.
-# echo "Warnings"
-# python3 -m pep8 --select "E501" ./flintrock || true
