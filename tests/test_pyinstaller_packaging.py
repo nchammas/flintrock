85afbba90de3ce58b4f38638d6a42e4b74520f46
Nicholas Chammas <nicholas.chammas@gmail.com>
2016-02-04 21:51:46 -0500

    don't install reqs as part of test

--- tests/test_pyinstaller_packaging.py
+++ tests/test_pyinstaller_packaging.py
@@ -6,11 +6,14 @@ import sys
 import pytest
 
 
+def pyinstaller_exists():
+    s = subprocess.run(['command', '-v', 'pyinstaller'])
+    return s.returncode == 0
+
+
 @pytest.mark.skipif(sys.version_info < (3, 5), reason="Python 3.5+ is required")
+@pytest.mark.skipif(not pyinstaller_exists(), reason="PyInstaller is required")
 def test_pyinstaller_packaging():
-    subprocess.run(
-        ['pip', 'install', '-r', 'requirements/maintainer.pip'],
-        check=True)
     subprocess.run(
         ['python', 'generate-standalone-package.py'],
         check=True)
