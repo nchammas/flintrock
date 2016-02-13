eca59fc0052874d9aa48b7d4d7d79192b5e609d1
Nicholas Chammas <nicholas.chammas@gmail.com>
2016-02-12 22:26:48 -0500

    retry Spark download when necessary

--- flintrock/scripts/install-spark.sh
+++ flintrock/scripts/install-spark.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
 
 spark_version="$1"
 distribution="$2"
@@ -9,7 +11,25 @@ echo "  distribution: ${distribution}"
 
 file="spark-${spark_version}-bin-${distribution}.tgz"
 
-curl --silent --remote-name "https://s3.amazonaws.com/spark-related-packages/${file}"
+# S3 is generally reliable, but sometimes when launching really large
+# clusters it can hiccup on us, in which case we'll need to retry the
+# download.
+set +e
+tries=1
+while true; do
+    curl --remote-name "https://s3.amazonaws.com/spark-related-packages/${file}"
+    curl_ret=$?
+
+    if ((curl_ret == 0)); then
+        break
+    elif ((tries >= 3)); then
+        exit 1
+    else
+        tries=$((tries + 1))
+    fi
+done
+set -e
+
 gzip -t "$file"
 
 mkdir "spark"
