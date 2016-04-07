b99b55ed2d92b7f63f9885ae431e7f6f9b5c069f
rmessner <rmessner@users.noreply.github.com>
2016-04-07 16:53:21 +0200

    Add security rule: open port 7077 from client

--- flintrock/ec2.py
+++ flintrock/ec2.py
@@ -367,6 +367,12 @@ def get_or_create_ec2_security_groups(
             from_port=4040,
             to_port=4040,
             cidr_ip=flintrock_client_cidr,
+            src_group=None),
+        SecurityGroupRule(
+            ip_protocol='tcp',
+            from_port=7077,
+            to_port=7077,
+            cidr_ip=flintrock_client_cidr,
             src_group=None)
     ]
 
