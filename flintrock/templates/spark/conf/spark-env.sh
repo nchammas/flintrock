b4bd82cc3cb5e72c2fd301510db7570326ce3086
Fernando Luiz Parisotto <fernando.parisotto@chaordicsystems.com>
2018-03-29 16:20:10 -0300

    private vpc

--- flintrock/templates/spark/conf/spark-env.sh
+++ flintrock/templates/spark/conf/spark-env.sh
@@ -13,8 +13,14 @@ export SPARK_MASTER_HOST="{master_host}"
 export HADOOP_CONF_DIR="$HOME/hadoop/conf"
 
 # TODO: Make this non-EC2-specific.
-# Bind Spark's web UIs to this machine's public EC2 hostname
-export SPARK_PUBLIC_DNS="$(curl --silent http://169.254.169.254/latest/meta-data/public-hostname)"
+# Bind Spark's web UIs to this machine's public EC2 hostname if it has, otherwise bind to private name
+HTTP_STATUS_PUBLIC_NAME=$(curl --head --silent --output /dev/null --write-out "%{{http_code}}" http://169.254.169.254/latest/meta-data/public-hostname)
+HTTP_STATUS_PRIVATE_NAME=$(curl --head --silent --output /dev/null --write-out "%{{http_code}}" http://169.254.169.254/latest/meta-data/hostname)
+if [ $HTTP_STATUS_PUBLIC_NAME -eq 200 ]; then
+    export SPARK_PUBLIC_DNS="$(curl --silent http://169.254.169.254/latest/meta-data/public-hostname)"
+elif [ $HTTP_STATUS_PRIVATE_NAME -eq 200  ]; then
+    export SPARK_PUBLIC_DNS="$(curl --silent http://169.254.169.254/latest/meta-data/hostname)"
+fi
 
 # TODO: Set a high ulimit for large shuffles
 # Need to find a way to do this, since "sudo ulimit..." doesn't fly.
