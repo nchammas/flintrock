d2318e99dd972765673a0b8d716d3409d337e2da
Fernando Luiz Parisotto <fernando.parisotto@chaordicsystems.com>
2018-06-20 10:33:01 -0300

    set min device size for ephemeral device (help to support m5d, c5d ecs instances)

--- flintrock/scripts/setup-ephemeral-storage.py
+++ flintrock/scripts/setup-ephemeral-storage.py
@@ -41,9 +41,10 @@ Mount = namedtuple(
 BlockDevice = namedtuple(
     'BlockDevice', [
         'name',
+        'size',
         'mount_point'
     ])
-BlockDevice.__new__.__defaults__ = (None, None)
+BlockDevice.__new__.__defaults__ = (None, 0, None)
 
 
 def get_non_root_block_devices():
@@ -52,11 +53,13 @@ def get_non_root_block_devices():
 
     These are the devices we're going to format and mount for use.
     """
+    min_size_of_disk_in_bytes = 10000000000
     block_devices_raw = subprocess.check_output([
         'lsblk',
         '--ascii',
         '--paths',
-        '--output', 'KNAME,MOUNTPOINT',
+        '-b',
+        '--output', 'KNAME,SIZE,MOUNTPOINT',
         # --inverse and --nodeps make sure that
         #   1) we get the mount points for devices that have holder devices
         #   2) we don't get the holder devices themselves
@@ -64,7 +67,7 @@ def get_non_root_block_devices():
         '--nodeps',
         '--noheadings']).decode('utf-8')
     block_devices = [BlockDevice(*line.split()) for line in block_devices_raw.splitlines()]
-    non_root_block_devices = [bd for bd in block_devices if bd.mount_point != '/']
+    non_root_block_devices = [bd for bd in block_devices if bd.mount_point != '/' and int(bd.size) >= min_size_of_disk_in_bytes]
     return non_root_block_devices
 
 
