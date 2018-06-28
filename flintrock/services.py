8298f3741208de379317972b4a9130c46df8deca
Nicholas Chammas <nicholas.chammas@gmail.com>
2018-06-28 01:53:36 -0400

    hardcode hadoop version when building spark from git

--- flintrock/services.py
+++ flintrock/services.py
@@ -318,7 +318,9 @@ class Spark(FlintrockService):
                 """.format(
                     repo=shlex.quote(self.git_repository),
                     commit=shlex.quote(self.git_commit),
-                    hadoop_short_version='.'.join(self.hadoop_version.split('.')[:2]),
+                    # Hardcoding this here until we figure out a better way to handle
+                    # the supported build profiles.
+                    hadoop_short_version='2.7',
                 ))
         ssh_check_output(
             client=ssh_client,
