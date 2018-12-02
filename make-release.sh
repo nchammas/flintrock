a81e89999fdf795404b7c13031f72dd8d4c0a3af
Nicholas Chammas <nicholas.chammas@gmail.com>
2018-12-02 14:01:54 -0500

    Update release script

--- make-release.sh
+++ make-release.sh
@@ -1,7 +1,8 @@
 # Update:
-#   - Default Spark and Hadoop versions
-#   - Default Amazon Linux AMI
-#   - Dependencies
+#   - Default Spark version: https://spark.apache.org/downloads.html
+#   - Default Hadoop version: https://hadoop.apache.org/releases.html
+#   - Default Amazon Linux 2 EBS AMI: https://aws.amazon.com/amazon-linux-2/release-notes/
+#   - Dependencies: https://requires.io/github/nchammas/flintrock/requirements/?branch=master
 # Run full acceptance tests
 # Update Flintrock version
 #   - flintrock/__init__.py
@@ -16,7 +17,13 @@ python setup.py sdist bdist_wheel
 
 # python setup.py register -r https://testpypi.python.org/pypi
 
+# Test PyPI upload
 twine upload dist/* --repository pypitest
+open https://test.pypi.org/project/Flintrock/
+
+# Production PyPI upload
+twine upload dist/* --repository pypi
+open https://pypi.org/project/Flintrock/
 
 python generate-standalone-package.py
 
@@ -30,10 +37,8 @@ python generate-standalone-package.py
 # ---
 
 # Test release via pip
-
 deactivate
 trash venv
-
 python3 -m venv venv
 source venv/bin/activate
 
