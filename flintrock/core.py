a1a7b0136b151f757e72a4cabae08522f4d3bc5a
Robert Laabs <robertlaabs@Roberts-MacBook-Pro.local>
2021-01-22 18:47:59 -0500

    Fixes to enable Flintrock to work when an EC2 instance is used as a host.

--- flintrock/core.py
+++ flintrock/core.py
@@ -619,7 +619,7 @@ def provision_cluster(
 
     master_ssh_client = get_ssh_client(
         user=user,
-        host=cluster.master_host,
+        host=cluster.master_ip,
         identity_file=identity_file)
 
     with master_ssh_client:
@@ -644,7 +644,7 @@ def provision_cluster(
                 cluster=cluster)
 
     for service in services:
-        service.health_check(master_host=cluster.master_host)
+        service.health_check(master_host=cluster.master_ip)
 
 
 def provision_node(
