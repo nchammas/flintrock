c91dff4ec08f37a31475914971907d394c448bad
Nicholas Chammas <nicholas.chammas@gmail.com>
2021-02-20 21:03:36 -0500

    clarify dist.apache.org locations

--- flintrock/flintrock.py
+++ flintrock/flintrock.py
@@ -285,7 +285,8 @@ def cli(cli_context, config, provider, debug):
                   "URL to download Hadoop from. If an S3 URL, Flintrock will use the "
                   "AWS CLI from the cluster nodes to download it. "
                   "Flintrock will append the appropriate file name to the end "
-                  "of the URL based on the Apache release file names on dist.apache.org."
+                  "of the URL based on the Apache release file names here: "
+                  "https://dist.apache.org/repos/dist/release/hadoop/common/"
               ),
               default='https://www.apache.org/dyn/closer.lua?action=download&filename=hadoop/common/hadoop-{v}/',
               show_default=True,
@@ -305,7 +306,8 @@ def cli(cli_context, config, provider, debug):
                   "AWS CLI from the cluster nodes to download it. "
                   "Flintrock will append the appropriate file "
                   "name to the end of the URL based on the selected Hadoop version and "
-                  "Apache release file names on dist.apache.org."
+                  "Apache release file names here: "
+                  "https://dist.apache.org/repos/dist/release/spark/"
               ),
               default='https://www.apache.org/dyn/closer.lua?action=download&filename=spark/spark-{v}/',
               show_default=True,
